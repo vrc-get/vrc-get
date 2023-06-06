24a75c288da4452103727ffe955c725fdac45088
anatawa12 <anatawa12@icloud.com>
2023-06-06 22:01:29 +0900

    feat: vrc-get info package

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -2,7 +2,7 @@ use std::num::NonZeroU32;
 use std::path::PathBuf;
 use clap::{Parser, Subcommand};
 use serde::Serialize;
-use crate::commands::load_unity;
+use crate::commands::{load_env, load_unity};
 use crate::version::Version;
 
 /// Shows information for other program.
@@ -10,9 +10,10 @@ use crate::version::Version;
 #[command(author, version)]
 pub enum Info {
     Project(Project),
+    Package(Package),
 }
 
-multi_command!(Info is Project);
+multi_command!(Info is Project, Package);
 
 /// Show project information
 #[derive(Parser)]
@@ -39,7 +40,7 @@ impl Project {
             supported @ 1..=1 => supported,
             unsupported => exit_with!("unsupported json version: {unsupported}"),
         };
-        
+
         debug_assert_eq!(format_version, 1);
 
         #[derive(Serialize)]
@@ -79,5 +80,56 @@ impl Project {
         };
 
         println!("{}", serde_json::to_string(&project).unwrap());
+    }
+}
+
+/// Show project information
+#[derive(Parser)]
+#[command(author, version)]
+pub struct Package {
+    #[arg()]
+    package: String,
+    #[command(flatten)]
+    env_args: super::EnvArgs,
+
+    /// Output json format
+    #[arg(long = "json-format")]
+    json_format: Option<NonZeroU32>,
+}
+
+impl Package {
+    pub async fn run(self) {
+        let env = load_env(&self.env_args).await;
+
+        let format_version = match self.json_format.map(|x| x.get()).unwrap_or_default() {
+            0 => {
+                eprintln!("warning: no --json-format is specified! using lastest version 1");
+                1
+            }
+            supported @ 1..=1 => supported,
+            unsupported => exit_with!("unsupported json version: {unsupported}"),
+        };
+
+        debug_assert_eq!(format_version, 1);
+
+        let packages = env.find_packages(&self.package);
+
+        let versions: Vec<_> = packages.iter().map(|x| PackageVersionInfo {version: x.version()}).collect();
+
+        #[derive(Serialize)]
+        struct PackageInfo<'a> {
+            versions: &'a [PackageVersionInfo<'a>]
+        }
+
+        #[derive(Serialize)]
+        struct PackageVersionInfo<'a> {
+            version: &'a Version
+        }
+
+        let package_info = PackageInfo {
+            versions: versions.as_slice()
+        };
+
+        println!("{}", serde_json::to_string(&package_info).unwrap());
     }
 }
