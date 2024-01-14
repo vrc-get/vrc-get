95daab032f6b2ce14bbb59f2cd96dc76f4a0f897
anatawa12 <anatawa12@icloud.com>
2024-01-14 22:11:30 +0900

    chore: remove litedb cli which was only for test

--- vrc-get-litedb/src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::env::args;
-
-fn main() {
-    let mut args = args();
-    let a = args.next().unwrap().parse().unwrap();
-    let b = args.next().unwrap().parse().unwrap();
-    unsafe { vrc_get_litedb::add_dotnet(a, b) };
-}
