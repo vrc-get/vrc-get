a9aac176237f552904fe8cf29e8c60f2aacc55f4
anatawa12 <anatawa12@icloud.com>
2024-02-05 01:20:09 +0900

    feat: add ObjectId constructor without bytes

--- vrc-get-litedb/src/bson.rs
+++ vrc-get-litedb/src/bson.rs
@@ -1,4 +1,8 @@
 use std::fmt::{Debug, Formatter};
+use std::sync::atomic::{AtomicU32, Ordering};
+use std::time::{SystemTime, UNIX_EPOCH};
+use once_cell::sync::Lazy;
+use rand::Rng;
 
 /// ObjectId in Bson. Used for identifying documents in a collection.
 #[repr(C)]
@@ -8,7 +12,48 @@ pub struct ObjectId {
 }
 
 impl ObjectId {
-    pub fn new(bytes: &[u8; 12]) -> Self {
+    pub fn new() -> ObjectId {
+        let timestamp = ObjectId::gen_timestamp();
+        let process_id = ObjectId::gen_process_id();
+        let counter = ObjectId::gen_count();
+
+        let mut buf: [u8; 12] = [0; 12];
+
+        buf[0..][..4].clone_from_slice(&timestamp[..]);
+        buf[4..][..5].clone_from_slice(&process_id[..]);
+        buf[9..][..3].clone_from_slice(&counter[..]);
+
+        ObjectId::from_bytes(&buf)
+    }
+
+    fn gen_timestamp() -> [u8; 4] {
+        let timestamp: u32 = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+            as u32;
+        timestamp.to_be_bytes()
+    }
+
+    fn gen_process_id() -> [u8; 5] {
+        static BUF: Lazy<[u8; 5]> = Lazy::new(rand::random);
+
+        *BUF
+    }
+
+    fn gen_count() -> [u8; 3] {
+        static OID_COUNTER: Lazy<AtomicU32> =
+            Lazy::new(|| AtomicU32::new(rand::thread_rng().gen_range(0..=0xFF_FF_FF)));
+        let u_counter = OID_COUNTER.fetch_add(1, Ordering::SeqCst);
+        let u_int = u_counter % 0x1_00_00_00;
+        let buf = u_int.to_be_bytes();
+        let buf_u24: [u8; 3] = [buf[1], buf[2], buf[3]];
+        buf_u24
+    }
+}
+
+impl ObjectId {
+    pub fn from_bytes(bytes: &[u8; 12]) -> Self {
         Self { bytes: *bytes }
     }
 }
@@ -26,6 +71,14 @@ impl Debug for ObjectId {
 #[repr(transparent)]
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct DateTime(u64);
+
+impl DateTime {
+    pub fn now() -> DateTime {
+        let now = SystemTime::now();
+        let duration = now.duration_since(UNIX_EPOCH).unwrap();
+        Self(duration.as_millis() as u64)
+    }
+}
 
 impl DateTime {
     pub fn from_millis_since_epoch(millis: u64) -> Self {
--- vrc-get-litedb/src/connection.rs
+++ vrc-get-litedb/src/connection.rs
@@ -127,7 +127,7 @@ mod tests {
         // {"_id":{"$oid":"65be38dfcbac18126a694ab2"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2022 worlds","Type":7,"UnityVersion":"2022.3.6f1","Favorite":false,"CreatedAt":{"$date":"2024-02-03T13:00:15.8020000Z"},"LastModified":{"$date":"2024-02-03T13:00:15.8020000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbe\x38\xdf\xcb\xac\x18\x12\x6a\x69\x4a\xb2"),
+            ObjectId::from_bytes(b"\x65\xbe\x38\xdf\xcb\xac\x18\x12\x6a\x69\x4a\xb2"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2022 worlds",
             ProjectType::WORLDS,
             Some("2022.3.6f1"),
@@ -138,7 +138,7 @@ mod tests {
         // {"_id":{"$oid":"65be38f3cbac18126a694ab3"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2022 avatars","Type":8,"UnityVersion":"2022.3.6f1","Favorite":false,"CreatedAt":{"$date":"2024-02-03T13:00:35.8090000Z"},"LastModified":{"$date":"2024-02-03T13:00:35.8090000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbe\x38\xf3\xcb\xac\x18\x12\x6a\x69\x4a\xb3"),
+            ObjectId::from_bytes(b"\x65\xbe\x38\xf3\xcb\xac\x18\x12\x6a\x69\x4a\xb3"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2022 avatars",
             ProjectType::AVATARS,
             Some("2022.3.6f1"),
@@ -149,7 +149,7 @@ mod tests {
         // {"_id":{"$oid":"65be391ecbac18126a694ab4"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 avatars","Type":8,"UnityVersion":"2019.4.31f1","Favorite":false,"CreatedAt":{"$date":"2024-02-03T13:01:18.7600000Z"},"LastModified":{"$date":"2024-02-03T13:01:18.7600000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbe\x39\x1e\xcb\xac\x18\x12\x6a\x69\x4a\xb4"),
+            ObjectId::from_bytes(b"\x65\xbe\x39\x1e\xcb\xac\x18\x12\x6a\x69\x4a\xb4"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 avatars",
             ProjectType::AVATARS,
             Some("2019.4.31f1"),
@@ -160,7 +160,7 @@ mod tests {
         // {"_id":{"$oid":"65be394bcbac18126a694ab5"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 worlds","Type":7,"UnityVersion":"2019.4.31f1","Favorite":false,"CreatedAt":{"$date":"2024-02-03T13:02:03.1890000Z"},"LastModified":{"$date":"2024-02-03T13:02:03.1890000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbe\x39\x4b\xcb\xac\x18\x12\x6a\x69\x4a\xb5"),
+            ObjectId::from_bytes(b"\x65\xbe\x39\x4b\xcb\xac\x18\x12\x6a\x69\x4a\xb5"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 worlds",
             ProjectType::WORLDS,
             Some("2019.4.31f1"),
@@ -171,7 +171,7 @@ mod tests {
         // {"_id":{"$oid":"65be3d65cbac18126a694ab7"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 unknown","Type":0,"UnityVersion":"2019.4.31f1","Favorite":false,"CreatedAt":{"$date":"2024-02-03T13:19:33.5020000Z"},"LastModified":{"$date":"2024-02-03T13:19:33.5020000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbe\x3d\x65\xcb\xac\x18\x12\x6a\x69\x4a\xb7"),
+            ObjectId::from_bytes(b"\x65\xbe\x3d\x65\xcb\xac\x18\x12\x6a\x69\x4a\xb7"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 unknown",
             ProjectType::UNKNOWN,
             Some("2019.4.31f1"),
@@ -182,7 +182,7 @@ mod tests {
         // {"_id":{"$oid":"65be3f75cbac18126a694ab8"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 legacy avatars","Type":3,"UnityVersion":"2019.4.31f1","Favorite":false,"CreatedAt":{"$date":"2024-02-03T13:28:21.9920000Z"},"LastModified":{"$date":"2024-02-03T13:28:21.9920000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbe\x3f\x75\xcb\xac\x18\x12\x6a\x69\x4a\xb8"),
+            ObjectId::from_bytes(b"\x65\xbe\x3f\x75\xcb\xac\x18\x12\x6a\x69\x4a\xb8"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 legacy avatars",
             ProjectType::LEGACY_AVATARS,
             Some("2019.4.31f1"),
@@ -193,7 +193,7 @@ mod tests {
         // {"_id":{"$oid":"65be3fff9854f50fadcd90bc"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 legacy worlds","Type":2,"UnityVersion":"2019.4.31f1","Favorite":false,"CreatedAt":{"$date":"2024-02-03T13:30:39.3360000Z"},"LastModified":{"$date":"2024-02-03T13:30:39.3360000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbe\x3f\xff\x98\x54\xf5\x0f\xad\xcd\x90\xbc"),
+            ObjectId::from_bytes(b"\x65\xbe\x3f\xff\x98\x54\xf5\x0f\xad\xcd\x90\xbc"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 legacy worlds",
             ProjectType::LEGACY_WORLDS,
             Some("2019.4.31f1"),
@@ -204,7 +204,7 @@ mod tests {
         // {"_id":{"$oid":"65be40449854f50fadcd90bd"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 vpm starter","Type":9,"UnityVersion":"2019.4.31f1","Favorite":false,"CreatedAt":{"$date":"2024-02-03T13:31:48.8900000Z"},"LastModified":{"$date":"2024-02-03T13:31:48.8900000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbe\x40\x44\x98\x54\xf5\x0f\xad\xcd\x90\xbd"),
+            ObjectId::from_bytes(b"\x65\xbe\x40\x44\x98\x54\xf5\x0f\xad\xcd\x90\xbd"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 vpm starter",
             ProjectType::VPM_STARTER,
             Some("2019.4.31f1"),
@@ -215,7 +215,7 @@ mod tests {
         // {"_id":{"$oid":"65bf19d67697f911929636a8"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 sdk2","Type":1,"UnityVersion":"2019.4.31f1","Favorite":false,"CreatedAt":{"$date":"2024-02-04T05:00:06.3190000Z"},"LastModified":{"$date":"2024-02-04T05:00:06.3190000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbf\x19\xd6\x76\x97\xf9\x11\x92\x96\x36\xa8"),
+            ObjectId::from_bytes(b"\x65\xbf\x19\xd6\x76\x97\xf9\x11\x92\x96\x36\xa8"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 sdk2",
             ProjectType::LEGACY_SDK2,
             Some("2019.4.31f1"),
@@ -226,7 +226,7 @@ mod tests {
         // {"_id":{"$oid":"65bf2e42cd9c24053deee1be"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 upm avatars","Type":5,"UnityVersion":"2019.4.31f1","Favorite":false,"CreatedAt":{"$date":"2024-02-04T06:27:14.8080000Z"},"LastModified":{"$date":"2024-02-04T06:27:14.8080000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbf\x2e\x42\xcd\x9c\x24\x05\x3d\xee\xe1\xbe"),
+            ObjectId::from_bytes(b"\x65\xbf\x2e\x42\xcd\x9c\x24\x05\x3d\xee\xe1\xbe"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 upm avatars",
             ProjectType::UPM_AVATARS,
             Some("2019.4.31f1"),
@@ -237,7 +237,7 @@ mod tests {
         // {"_id":{"$oid":"65bf2e4fcd9c24053deee1bf"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 upm worlds","Type":4,"UnityVersion":"2019.4.31f1","Favorite":false,"CreatedAt":{"$date":"2024-02-04T06:27:27.3630000Z"},"LastModified":{"$date":"2024-02-04T06:27:27.3630000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbf\x2e\x4f\xcd\x9c\x24\x05\x3d\xee\xe1\xbf"),
+            ObjectId::from_bytes(b"\x65\xbf\x2e\x4f\xcd\x9c\x24\x05\x3d\xee\xe1\xbf"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 upm worlds",
             ProjectType::UPM_WORLDS,
             Some("2019.4.31f1"),
@@ -248,7 +248,7 @@ mod tests {
         // {"_id":{"$oid":"65bf2e56cd9c24053deee1c0"},"Path":"C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 upm starter","Type":6,"UnityVersion":"2019.4.31f1","Favorite":false,"CreatedAt":{"$date":"2024-02-04T06:27:34.2590000Z"},"LastModified":{"$date":"2024-02-04T06:27:34.2590000Z"}}
         check_exists(
             &projects,
-            ObjectId::new(b"\x65\xbf\x2e\x56\xcd\x9c\x24\x05\x3d\xee\xe1\xc0"),
+            ObjectId::from_bytes(b"\x65\xbf\x2e\x56\xcd\x9c\x24\x05\x3d\xee\xe1\xc0"),
             "C:\\Users\\anata\\AppData\\Local\\VRChatProjects\\VCC Config Test 2019 upm starter",
             ProjectType::UPM_STARTER,
             Some("2019.4.31f1"),
