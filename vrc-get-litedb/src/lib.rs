f3a0097572c8b17ab1e91807b8413dc212c95c94
anatawa12 <anatawa12@icloud.com>
2024-02-09 21:39:08 +0900

    feat: expose more items at root

--- vrc-get-litedb/src/lib.rs
+++ vrc-get-litedb/src/lib.rs
@@ -1,12 +1,19 @@
 mod bson;
 pub mod connection;
 mod connection_string; // exported in connection
-pub mod error;
+mod error;
 mod lowlevel;
-pub mod project;
+mod project;
 mod unity_version;
 
 pub use bson::DateTime;
 pub use bson::ObjectId;
+pub use connection::ConnectionString;
+pub use connection::DatabaseConnection;
+pub use error::Error;
+pub use error::ErrorKind;
+pub use project::Project;
+pub use project::ProjectType;
+pub use unity_version::UnityVersion;
 
-pub type Result<T> = std::result::Result<T, error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
