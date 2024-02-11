5d73976a844ca45ef54bb04ea46477111d227a6f
anatawa12 <anatawa12@icloud.com>
2024-02-11 15:17:03 +0900

    chore: temporary use vrc-get-litedb from relative path

--- vrc-get/build.rs
+++ vrc-get/build.rs
@@ -8,13 +8,6 @@ fn main() {
     if std::env::var("CARGO_FEATURE_EXPERIMENTAL_VCC").is_ok() {
         let target_info = TargetInformation::from_triple(std::env::var("TARGET").unwrap().as_str());
 
-        println!("cargo:rustc-link-arg={home}/.nuget/packages/runtime.{target}.microsoft.dotnet.ilcompiler/{version}/sdk/{lib}",
-                 home = home::home_dir().unwrap().to_str().unwrap(),
-                 target = target_info.dotnet_runtime_id,
-                 version = FRAMEWORK_VERSION,
-                 lib = target_info.bootstrapper
-        );
-
         if target_info.family == TargetFamily::Linux {
             // start stop gc is not supported by dotnet.
             println!("cargo:rustc-link-arg=-Wl,-z,nostart-stop-gc");
