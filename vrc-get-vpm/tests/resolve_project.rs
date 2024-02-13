ee233dd90bda613d1cd60e2f976f36af842e95b4
anatawa12 <anatawa12@icloud.com>
2024-02-13 23:08:55 +0900

    test: resolving dependencies without locked

--- vrc-get-vpm/tests/resolve_project.rs
+++ vrc-get-vpm/tests/resolve_project.rs
@@ -44,3 +44,37 @@ fn simple_resolve_fully_locked() {
         assert_install_only(&result, &library);
     })
 }
+
+#[test]
+fn resolve_ranged() {
+    block_on(async {
+        let project = VirtualProjectBuilder::new()
+            .add_dependency_range("com.anatawa12.package", "^1.0.0")
+            .build()
+            .await
+            .unwrap();
+
+        let collection = PackageCollectionBuilder::new()
+            .add(
+                PackageJson::new("com.anatawa12.package", Version::new(1, 0, 0))
+                    .add_vpm_dependency("com.anatawa12.library", "1.0.0"),
+            )
+            .add(PackageJson::new(
+                "com.anatawa12.library",
+                Version::new(1, 0, 0),
+            ))
+            .build();
+
+        let result = project.resolve_request(&collection).await.unwrap();
+
+        assert_eq!(result.package_changes().len(), 2);
+        assert_eq!(result.remove_legacy_folders().len(), 0);
+        assert_eq!(result.remove_legacy_files().len(), 0);
+        assert_eq!(result.conflicts().len(), 0);
+
+        let package = collection.get_package("com.anatawa12.package", Version::new(1, 0, 0));
+        let library = collection.get_package("com.anatawa12.library", Version::new(1, 0, 0));
+        assert_installing_to_both(&result, &package);
+        assert_installing_to_locked_only(&result, &library);
+    })
+}
