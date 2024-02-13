fb8ae2f8f0fbb996a37674af23f4a6f9fcb2f8e4
anatawa12 <anatawa12@icloud.com>
2024-02-13 17:59:58 +0900

    test: transitive_unused_remove

--- vrc-get-vpm/tests/remove_package.rs
+++ vrc-get-vpm/tests/remove_package.rs
@@ -30,3 +30,39 @@ fn basic_remove() {
         assert_eq!(gists_change.reason(), RemoveReason::Requested);
     })
 }
+
+#[test]
+fn transitive_unused_remove() {
+    block_on(async {
+        // create minimum project
+        let project = VirtualProjectBuilder::new()
+            .add_dependency("com.vrchat.avatars", Version::new(1, 0, 0))
+            .add_locked(
+                "com.vrchat.avatars",
+                Version::new(1, 0, 0),
+                &[("com.vrchat.base", "1.0.0")],
+            )
+            .add_locked("com.vrchat.base", Version::new(1, 0, 0), &[])
+            .build()
+            .await
+            .unwrap();
+
+        let result = project
+            .remove_request(&[&"com.vrchat.avatars"])
+            .await
+            .unwrap();
+
+        assert_eq!(result.package_changes().len(), 2);
+        assert_eq!(result.remove_legacy_folders().len(), 0);
+        assert_eq!(result.remove_legacy_files().len(), 0);
+        assert_eq!(result.conflicts().len(), 0);
+
+        let avatars_change = result.package_changes().get("com.vrchat.avatars").unwrap();
+        let avatars_change = avatars_change.as_remove().expect("avatars is not removing");
+        assert_eq!(avatars_change.reason(), RemoveReason::Requested);
+
+        let avatars_change = result.package_changes().get("com.vrchat.base").unwrap();
+        let avatars_change = avatars_change.as_remove().expect("base is not removing");
+        assert_eq!(avatars_change.reason(), RemoveReason::Unused);
+    })
+}
