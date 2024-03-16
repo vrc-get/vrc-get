63e2914bb7fc97f743ed6beabcf3e28443fe46c7
anatawa12 <anatawa12@icloud.com>
2024-03-16 19:39:48 +0900

    feat(gui): i18n toasts

--- vrc-get-gui/lib/unsupported.ts
+++ vrc-get-gui/lib/unsupported.ts
@@ -1,5 +1,6 @@
 import {toast} from "react-toastify";
+import i18next from "@/lib/i18n";
 
 export function unsupported(feature: string): () => void {
-	return () => toast.error(`${feature} is not supported yet`)
+	return () => toast.error(i18next.t("{{name}} is not supported yet", {name: feature}))
 }
