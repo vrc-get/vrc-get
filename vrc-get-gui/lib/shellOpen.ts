20be7bb3a9f4f719ce370d2134ac867a8257d49c
anatawa12 <anatawa12@icloud.com>
2024-03-29 00:58:04 +0900

    chore: remove lazy import from shellOpen

--- vrc-get-gui/lib/shellOpen.ts
+++ vrc-get-gui/lib/shellOpen.ts
@@ -1,4 +1,6 @@
+import {open} from "@tauri-apps/api/shell";
+
 export async function shellOpen(url: string) {
 	// since @tauri-apps/api uses navigator while importing, we need to import it in a function to avoid unexpected SSR
-	await (await import("@tauri-apps/api")).shell.open(url);
+	await open(url);
 }
