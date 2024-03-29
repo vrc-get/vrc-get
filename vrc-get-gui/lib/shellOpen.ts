f8c34de33552d83c186d893ff066108701ca6a2d
anatawa12 <anatawa12@icloud.com>
2024-03-29 15:00:22 +0900

    fix: incorrect comment

--- vrc-get-gui/lib/shellOpen.ts
+++ vrc-get-gui/lib/shellOpen.ts
@@ -1,6 +1,5 @@
 import {open} from "@tauri-apps/api/shell";
 
 export async function shellOpen(url: string) {
-	// since @tauri-apps/api uses navigator while importing, we need to import it in a function to avoid unexpected SSR
 	await open(url);
 }
