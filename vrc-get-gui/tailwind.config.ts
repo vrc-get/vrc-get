2c5e084a71b8e19f2c1c4ecb43d8fc36b88af17e
anatawa12 <anatawa12@icloud.com>
2024-05-16 10:17:23 +0900

    chore: remove bundling fonts and use system font instead

--- vrc-get-gui/tailwind.config.ts
+++ vrc-get-gui/tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			fontFamily: {
+				sans: ["system-ui"],
+				mono: ["monospace"],
+			}
 		},
 	},
 	plugins: [],
