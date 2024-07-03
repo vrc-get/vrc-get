74f2205dedaa96bf4d64d6df601f6f2b4880ef5f
anatawa12 <anatawa12@icloud.com>
2024-07-03 15:44:09 +0900

    refactor: remove unnecessary VStacks

--- vrc-get-gui/app/settings/licenses/render-client.tsx
+++ vrc-get-gui/app/settings/licenses/render-client.tsx
@@ -17,35 +17,33 @@ export default function RenderPage({licenses}: { licenses: Licenses | null }) {
 	}
 
 	return (
-		<VStack>
-			<ScrollPageContainer>
-				<VStack className={"gap-4"}>
-					<Card className={"p-4"}>
-						<p>
-							This project is built on top of many open-source projects.<br/>
-							Here are the licenses of the projects used in this project:
-						</p>
-						<ul>
-						</ul>
-					</Card>
+		<ScrollPageContainer>
+			<VStack>
+				<Card className={"p-4"}>
+					<p>
+						This project is built on top of many open-source projects.<br/>
+						Here are the licenses of the projects used in this project:
+					</p>
+					<ul>
+					</ul>
+				</Card>
 
-					{licenses.map((license, idx) => (
-						<Card className={"p-4"} key={idx}>
-							<h3>{license.name}</h3>
-							<h4>Used by:</h4>
-							<ul className={"ml-2"}>
-								{license.packages.map(pkg => (
-									<li key={`${pkg.name}@${pkg.version}`}><a
-										onClick={() => shellOpen(pkg.url)}>{pkg.name} ({pkg.version})</a></li>
-								))}
-							</ul>
-							<ScrollableCard className="max-h-52">
-								<pre className={"whitespace-pre-wrap"}>{license.text}</pre>
-							</ScrollableCard>
-						</Card>
-					))}
-				</VStack>
-			</ScrollPageContainer>
-		</VStack>
+				{licenses.map((license, idx) => (
+					<Card className={"p-4"} key={idx}>
+						<h3>{license.name}</h3>
+						<h4>Used by:</h4>
+						<ul className={"ml-2"}>
+							{license.packages.map(pkg => (
+								<li key={`${pkg.name}@${pkg.version}`}><a
+									onClick={() => shellOpen(pkg.url)}>{pkg.name} ({pkg.version})</a></li>
+							))}
+						</ul>
+						<ScrollableCard className="max-h-52">
+							<pre className={"whitespace-pre-wrap"}>{license.text}</pre>
+						</ScrollableCard>
+					</Card>
+				))}
+			</VStack>
+		</ScrollPageContainer>
 	);
 }
