2b83ba1724191be74e45e5c2911bc4fec6c2ab80
anatawa12 <anatawa12@icloud.com>
2024-08-10 18:28:44 +0900

    feat: mutex file synchronization for windows

--- vrc-get-vpm/src/environment/litedb.rs
+++ vrc-get-vpm/src/environment/litedb.rs
@@ -6,6 +6,7 @@ use vrc_get_litedb::DatabaseConnection;
 
 pub struct VccDatabaseConnection {
     pub(crate) db: DatabaseConnection,
+    _guard: has_drop::MutexGuard,
 }
 
 impl VccDatabaseConnection {
@@ -13,12 +14,18 @@ impl VccDatabaseConnection {
         io.connect_lite_db().await
     }
 
-    pub(crate) fn new(db: DatabaseConnection) -> Self {
-        Self { db }
+    pub(crate) fn new(db: DatabaseConnection, _guard: has_drop::MutexGuard) -> Self {
+        Self { db, _guard }
     }
 
     pub async fn save(&self, _: &impl EnvironmentIo) -> io::Result<()> {
         // nop for now but might have to do something in the future
         Ok(())
     }
+}
+
+mod has_drop {
+    pub type MutexGuard = Box<dyn HasDrop>;
+    pub trait HasDrop: Send + Sync {}
+    impl<T: ?Sized + Send + Sync> HasDrop for T {}
 }
