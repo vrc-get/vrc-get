033e1c5dbe4fd789a61dd803439b10b48b0301bd
anatawa12 <github@anatawa12.com>
2024-09-03 04:58:03 +0900

    fix: add polyfill for ResizeObserver

--- vrc-get-gui/components/providers.tsx
+++ vrc-get-gui/components/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "resize-observer-polyfill";
+import "@/lib/polyfill";
 import { CheckForUpdateMessage } from "@/components/CheckForUpdateMessage";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import type { CheckForUpdateResponse, LogEntry } from "@/lib/bindings";
