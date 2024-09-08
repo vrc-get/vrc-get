e1c7106802ce345b954df278e7745f6cee40df06
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2024-09-08 16:45:35 +0200

    fix lint issues.

--- vrc-get-gui/app/(main)/settings/page.tsx
+++ vrc-get-gui/app/(main)/settings/page.tsx
@@ -467,8 +467,8 @@ function FilesAndFoldersCard() {
 				</Button>
 			</div>
 		</Card>
-			);
-};
+	);
+}
 
 function AlcomCard({
 	isMac,
@@ -538,8 +538,6 @@ function AlcomCard({
 			toastThrownError(e);
 		}
 	};
-
-
 
 	return (
 		<Card className={"flex-shrink-0 p-4 flex flex-col gap-4"}>
