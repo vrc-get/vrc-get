59535943d6f706890e0dd7228230824b39cb5e56
RingLo_ <63995099+lonelyicer@users.noreply.github.com>
2024-12-12 02:15:32 +0800

    chore: licenses page improved

--- vrc-get-gui/app/(main)/settings/licenses/page.tsx
+++ vrc-get-gui/app/(main)/settings/licenses/page.tsx
@@ -1,8 +1,10 @@
 import { loadLicenses } from "@/lib/licenses";
 import RenderPage from "./render-client";
+import Loading from "@/app/loading";
+import React, {Suspense} from "react";
 
 const licenses = await loadLicenses();
 
 export default function Page() {
-	return <RenderPage licenses={licenses} />;
+	return <Suspense fallback={<Loading />}><RenderPage licenses={licenses} /></Suspense>;
 }
