da5f6e564bd505c8fabe1fa8d501796838e608ca
anatawa12 <anatawa12@icloud.com>
2024-12-21 12:22:24 +0900

    chore: improve button at edge of header

--- vrc-get-gui/components/layout.tsx
+++ vrc-get-gui/components/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import type React from "react";
 
 export function VStack({
@@ -17,22 +18,34 @@ export function HNavBar({
 	leading,
 	trailing,
 	commonClassName,
+	leadingClassName,
+	trailingClassName,
 }: {
 	className?: string;
 	leading: React.ReactNode;
 	trailing?: React.ReactNode;
 	commonClassName?: string;
+	leadingClassName?: string;
+	trailingClassName?: string;
 }) {
 	return (
 		<Card className={`${className} mx-auto px-4 py-2 w-full`}>
 			<div className="mx-auto flex flex-wrap items-center justify-between text-primary gap-2">
 				<div
-					className={`flex items-center gap-2 ${commonClassName} me-auto flex-grow flex-shrink`}
+					className={cn(
+						"flex items-center gap-2 me-auto flex-grow flex-shrink",
+						commonClassName,
+						leadingClassName,
+					)}
 				>
 					{leading}
 				</div>
 				<div
-					className={`flex items-center gap-2 ${commonClassName} ms-auto flex-wrap justify-end`}
+					className={cn(
+						"flex items-center gap-2 ms-auto flex-wrap justify-end -mr-1",
+						commonClassName,
+						trailingClassName,
+					)}
 				>
 					{trailing}
 				</div>
