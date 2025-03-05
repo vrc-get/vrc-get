5701de2dbe9054dc2a8e4f395962f6ea4255a100
anatawa12 <anatawa12@icloud.com>
2025-03-05 15:31:56 +0900

    feat: add get_executable_path and get_app_path

--- vrc-get-vpm/src/unity_hub.rs
+++ vrc-get-vpm/src/unity_hub.rs
@@ -3,13 +3,49 @@ mod os;
 
 use crate::io;
 use crate::version::UnityVersion;
+use std::borrow::Cow;
 use std::ffi::OsStr;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Output;
 use tokio::process::Command;
 
 pub use find_unity_from_unity_hub_logic::find_available_editors;
 pub use os::load_unity_version;
+
+/// Returns the path to executable file
+///
+/// On macOS, this function expects Path to .app file and returns binary file.
+/// On other platforms, does nothing
+pub fn get_executable_path(path: &Path) -> Cow<Path> {
+    #[cfg(not(target_os = "macos"))]
+    {
+        Cow::Borrowed(path)
+    }
+    #[cfg(target_os = "macos")]
+    {
+        Cow::Owned(path.join("Contents/MacOS/Unity"))
+    }
+}
+
+/// Returns the path to application path
+///
+/// On macOS, this function converts an executable file path to .app path.
+/// On other platforms, does nothing.
+pub fn get_app_path(path: &Path) -> &Path {
+    #[cfg(not(target_os = "macos"))]
+    {
+        path
+    }
+    #[cfg(target_os = "macos")]
+    {
+        if path.ends_with("Contents/MacOS/Unity") {
+            path.parent().unwrap().parent().unwrap().parent().unwrap()
+        } else {
+            // Fallback to normal path.
+            path
+        }
+    }
+}
 
 #[allow(dead_code)]
 async fn headless_unity_hub(unity_hub_path: &OsStr, args: &[&OsStr]) -> io::Result<Output> {
@@ -31,19 +67,8 @@ pub async fn get_unity_from_unity_hub(
 ) -> io::Result<Vec<(UnityVersion, PathBuf)>> {
     let mut result = Vec::new();
 
-    #[cfg(not(target_os = "macos"))]
-    fn unity_path(original: PathBuf) -> PathBuf {
-        original
-    }
-
-    #[cfg(target_os = "macos")]
-    fn unity_path(original: PathBuf) -> PathBuf {
-        // on macos, unity hub returns path to app bundle folder, not the executable
-        original.join("Contents/MacOS/Unity")
-    }
-
     for x in find_available_editors().await? {
-        result.push((x.version, unity_path(x.path)));
+        result.push((x.version, get_executable_path(&x.path).into_owned()));
     }
 
     Ok(result)
