63ec14a5dc06e548a9f939291d1ab7b88678e033
anatawa12 <anatawa12@icloud.com>
2025-03-21 23:41:23 +0900

    feat: add polyfills for Symbol.dispose

--- vrc-get-gui/lib/main.tsx
+++ vrc-get-gui/lib/main.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
+import "./polyfill";
 
 // Set up a Router instance
 const router = createRouter({
