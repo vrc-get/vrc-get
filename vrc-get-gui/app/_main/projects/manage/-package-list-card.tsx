9d532e4d6786ea29c940c41e1036ffdd086c520e
Sayamame-beans <61457993+Sayamame-beans@users.noreply.github.com>
2025-03-23 21:21:40 +0900

    chore(l10n): Apply suggestions from code review
    Co-authored-by: anatawa12 <anatawa12@icloud.com>

--- vrc-get-gui/app/_main/projects/manage/-package-list-card.tsx
+++ vrc-get-gui/app/_main/projects/manage/-package-list-card.tsx
@@ -481,7 +481,7 @@ function ManagePackagesHeading({
 			<DropdownMenu>
 				<DropdownMenuTrigger asChild>
 					<Button className={"shrink-0 p-3"}>
-						{tc("projects:manage:button:package filter")}
+						{tc("projects:manage:button:package filters")}
 					</Button>
 				</DropdownMenuTrigger>
 				<DropdownMenuContent
