3d81b4b37fde868cd3d33e071419374593836e2e
RingLo_ <63995099+lonelyicer@users.noreply.github.com>
2025-04-23 20:08:05 +0800

    lint(biome): fix

--- vrc-get-gui/app/_main/log/-logs-list-card.tsx
+++ vrc-get-gui/app/_main/log/-logs-list-card.tsx
@@ -37,7 +37,8 @@ export const LogsListCard = memo(function LogsListCard({
 
 				const container = scrollContainerRef.current;
 				const isNearBottom =
-					container.scrollHeight - (container.scrollTop + container.clientHeight) <
+					container.scrollHeight -
+						(container.scrollTop + container.clientHeight) <
 					50;
 
 				if (!isNearBottom) {
