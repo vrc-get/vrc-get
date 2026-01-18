be366861ef527c5319ca652ccc5241b068bdadfc
tukiminya <me@tuki.dev>
2026-01-19 01:00:32 +0900

    feat(vrc-get-gui): migrate button component to new-york style & set default value on variant, size props

--- vrc-get-gui/components/ui/button.tsx
+++ vrc-get-gui/components/ui/button.tsx
@@ -5,22 +5,23 @@ import type * as React from "react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
 	{
 		variants: {
 			variant: {
-				default:
-					"bg-primary text-primary-foreground hover:bg-primary/90 shadow-primary/50 hover:shadow-primary/50 shadow-sm hover:shadow-md transition-shadow uppercase",
+				default: "bg-primary text-primary-foreground hover:bg-primary/90",
 				destructive:
-					"bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-destructive/50 hover:shadow-destructive/50 shadow-sm hover:shadow-md transition-shadow uppercase",
+					"bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
 				warning:
 					"bg-warning text-warning-foreground hover:bg-warning/90 shadow-warning/50 hover:shadow-warning/50 shadow-sm hover:shadow-md transition-shadow uppercase",
 				"outline-success":
 					"border border-input hover:text-accent-foreground border-success hover:border-success/70 text-success hover:text-success/70",
+				outline:
+					"border bg-background sha:bg-warning/90ow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
 				secondary:
 					"bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-secondary/50 hover:shadow-secondary/50 shadow-sm hover:shadow-md transition-shadow uppercase",
 				ghost:
-					"hover:bg-accent text-accent-foreground hover:text-accent-foreground",
+					"hover:bg-accent text-accent-foreground dark:hover:bg-accent/50",
 				"ghost-destructive":
 					"hover:bg-destructive/10 text-destructive hover:text-destructive",
 				link: "text-primary underline-offset-4 hover:underline",
@@ -29,10 +30,12 @@ const buttonVariants = cva(
 					"bg-success text-success-foreground hover:bg-success/90 shadow-success/50 hover:shadow-success/50 shadow-sm hover:shadow-md transition-shadow uppercase",
 			},
 			size: {
-				default: "h-10 px-4 py-2 compact:h-8",
-				sm: "h-9 rounded-md px-3 compact:h-7",
-				lg: "h-11 rounded-md px-8 compact:h-9",
-				icon: "h-10 w-10 compact:h-8 compact:w-8",
+				default: "h-9 px-4 py-2 has-[>svg]:px-3",
+				sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+				lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+				icon: "size-9",
+				"icon-sm": "size-8",
+				"icon-lg": "size-10",
 			},
 		},
 		defaultVariants: {
@@ -42,27 +45,27 @@ const buttonVariants = cva(
 	},
 );
 
-export interface ButtonProps
-	extends React.ComponentProps<"button">,
-		VariantProps<typeof buttonVariants> {
-	asChild?: boolean;
-}
-
 const Button = ({
 	className,
 	variant,
 	size,
 	asChild = false,
 	...props
-}: ButtonProps) => {
+}: React.ComponentProps<"button"> &
+	VariantProps<typeof buttonVariants> & {
+		asChild?: boolean;
+	}) => {
 	const Comp = asChild ? Slot : "button";
+
 	return (
 		<Comp
+			data-slot="button"
+			data-variant={variant}
+			data-size={size}
 			className={cn(buttonVariants({ variant, size, className }))}
 			{...props}
 		/>
 	);
 };
-Button.displayName = "Button";
 
 export { Button, buttonVariants };
